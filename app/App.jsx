55dc09d1e5b9b082d19d4fa69f320c971da2e328
dannyradden <dradden@gmail.com>
2017-08-07 14:31:41 -0600

    finish calc app w/o styling

--- app/App.jsx
+++ app/App.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Frame from './frame';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -9,6 +10,7 @@ const App = () => (
   <BrowserRouter>
     <div className="app">
       <Switch>
+        <Route exact path="/" component={Frame} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
