dec6d60542cfbdc93e64ff8b0f207582f56c58e6
dannyradden <dradden@gmail.com>
2017-08-07 14:32:48 -0600

    Delete data from types.js

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -1,9 +1,1 @@
-// @flow
-
-declare var module: {
-  hot: {
-    accept(path: string, callback: () => void): void,
-  },
-}
-
-export type Show = { title: string, description: string, year: string, imdbID: string, trailer: string, poster: string }
+// Add flow types here
